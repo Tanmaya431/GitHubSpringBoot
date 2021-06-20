9ca6b4abab26238a0d0a1a52b037d0e1b30c0128
Tanmaya <Tanmaya@DESKTOP-NHAQPJ5>
2021-06-20 15:43:41 +0530

    Modified

--- CJ09-DemoProject/src/com/nt/beans/Utility.java
+++ CJ09-DemoProject/src/com/nt/beans/Utility.java
@@ -3,8 +3,12 @@ package com.nt.beans;
 public class Utility {
 	
 	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
 		
 	 System.out.println("Hellow World");
+	 System.out.println(a+b);
 	}
 
 }
